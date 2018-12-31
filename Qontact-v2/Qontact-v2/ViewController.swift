6fbb32e3c00491d1aa5a0f4f39b9b2ed6786f8fd
Rohin Chandra <rohin.chandra@menloschool.org>
2018-12-31 12:01:17 -0800

    Initial commit

---
+++ Qontact-v2/Qontact-v2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Qontact-v2
+//
+//  Created by Rob chandra on 12/31/18.
+//  Copyright © 2018 Rohin chandra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
